ea73ce7784683a7bbeaf40a1a8847c5d00091710
JpAcode <132785238+JpAcode@users.noreply.github.com>
2023-05-06 22:47:49 +0200

    Update Codevolatility.py

--- Codevolatility.py
+++ Codevolatility.py
@@ -4,61 +4,39 @@
 # In[ ]:
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import yfinance as yf
-import pandas as pd
-import plotly.express as px
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        ticker = request.form['ticker']
-        option_type = request.form['option_type']
-        start_date = request.form['start_date']
-        end_date = request.form['end_date']
-        return plot(ticker, option_type, start_date, end_date)
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
-def plot(ticker, option_type, start_date, end_date):
-    df = yf.download(ticker, start=start_date, end=end_date, group_by='Ticker')
-    options = df.options
-    chain = df.option_chain(options[0])
-    calls = chain.calls
-    puts = chain.puts
+@app.route('/pricer')
+def pricer():
+    return render_template('pricer.html')
 
-    if option_type == 'call':
-        data = calls
-    elif option_type == 'put':
-        data = puts
-    else:
-        return 'Invalid option type'
+@app.route('/curves_rates')
+def curves_rates():
+    return render_template('curves_rates.html')
 
-    data = data[data['inTheMoney'] == False]
+@app.route('/', methods=['POST'])
+def plot():
+    ticker = request.form['ticker']
+    start_date = request.form['start_date']
+    end_date = request.form['end_date']
+    option_type = request.form['option_type']
+    exp_date = request.form['exp_date']
 
-    strikes = data['strike'].unique()
-    expirations = data['expiration'].unique()
+    stock = yf.Ticker(ticker)
+    df = stock.option_chain(exp_date)[option_type]
+    df = df[(df['contractSymbol'].str[-1:] == 'C') | (df['contractSymbol'].str[-1:] == 'P')]
 
-    ivs = []
-    for s in strikes:
-        iv_row = []
-        for e in expirations:
-            option = data[(data['expiration'] == e) & (data['strike'] == s)]
-            if not option.empty:
-                iv_row.append(option['impliedVolatility'].iloc[0])
-            else:
-                iv_row.append(None)
-        ivs.append(iv_row)
+    # Plotting code here
+    plot_div = "Plot will be displayed here"
 
-    fig = px.surface(x=expirations, y=strikes, z=ivs)
-    fig.update_layout(title=f'Volatility Smile for {ticker.upper()} {option_type.capitalize()} Options',
-                      scene=dict(xaxis_title='Expiration', yaxis_title='Strike', zaxis_title='Implied Volatility'))
-
-    return render_template('plot.html', plot=fig.to_html(full_html=False))
-
+    return render_template('index.html', plot_div=plot_div)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
